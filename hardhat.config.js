01ac2b7d42a14be3177d5c93f8c0a090e481431c
iotadex <wangyi_j@163.com>
2023-03-02 22:09:47 +0800

    add hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "smrevm1070",
+  networks:{
+    smrevm1070:{
+      url:"https://json-rpc.evm.testnet.shimmer.network/",
+      accounts:[process.env.RMS_CONTRACT_PRIVATEKEY],
+    }
+  }
 };
